94ba0789bf1dfa0d7f54452c2a925f92f2c4be67
xiaoxia <xiaqiubo123@163.com>
2017-10-30 17:48:17 +0800

    修复中文文件夹404

--- http/static.js
+++ http/static.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const querystring = require('querystring')
 
 // 获取启动时传入的文件夹,默认为当前文件夹
 let staticDir = process.argv.slice(2)[0] || __dirname
@@ -9,8 +10,10 @@ let staticDir = process.argv.slice(2)[0] || __dirname
 http.createServer((req,res)=>{
 	// 书写响应头信息,声明返回的是html
 	res.writeHead(200,{'content-type':'text/html;charset=utf8'})
+	//	使用querstring解析路由传入的中文字符
+	let urlParse = Object.keys(querystring.parse(req.url))[0]
 	// 组合目录文件夹
-	let rootName = path.join(staticDir+'/'+req.url)
+	let rootName = path.join(staticDir+'/'+urlParse)
 
 	// 如果请求路由没有对应的文件或文件夹,返回404
 	if(!fs.existsSync(rootName)){
@@ -23,7 +26,7 @@ http.createServer((req,res)=>{
 	let dirArr
 	// 如果路径是文件夹则读取文件夹内容
 	if(fs.statSync(rootName).isDirectory()){
-		dirArr = openDir(path.join(staticDir+'/'+req.url)).join('')
+		dirArr = openDir(rootName).join('')
 		res.write(dirArr)
 		res.end()
 		return 
