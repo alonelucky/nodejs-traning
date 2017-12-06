376c9b52cc2dd734ca0e56c03f72dbff0589fc71
xiaoxia <xiaqiubo123@163.com>
2017-12-06 17:17:28 +0800

    简化文件内容,使用ReadLine的ouput

--- fs/readline/readline.js
+++ fs/readline/readline.js
@@ -1,12 +1,11 @@
 const fs = require("fs")
 const { createInterface } = require("readline")
 
-// 最终使用的文件
-let dbConfig = fs.createWriteStream("./db.config.js")
-
 // 获取模板的可写流,创建逐行读取,模板中有特定插入符号(此处为双下划线)
 let readline = createInterface({
-    input:fs.createReadStream("./db.example.js")
+    input:fs.createReadStream("./db.example.js"),
+    // 最终使用的文件
+    output:fs.createWriteStream("./db.config.js")
 })
 
 // 模拟数据
@@ -28,16 +27,16 @@ readline.on("line",(chunk)=>{
     let regResult = str.match(reg)
     // 如果没有匹配结果,则不处理直接写入文件
     if(!regResult){
-        dbConfig.write(chunk+"\n")
+        readline.output.write(chunk+"\n")
         return
     }
     // 如果有结果,处理结果(此处获取的结果比实际使用多一个冒号,所以截取去除)
     let keyStr = regResult[0].substr(0,regResult[0].length-1)
     // 处理并写入数据
-    dbConfig.write(str.replace("__",dbData[keyStr])+"\n")
+    readline.output.write(str.replace("__",dbData[keyStr])+"\n")
 })
 
 // 读取结束,关闭可写流
 readline.on("end",()=>{
-    dbConfig.close()
+    readline.output.close()
 })
